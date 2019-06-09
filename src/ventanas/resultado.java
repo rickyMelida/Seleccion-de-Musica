456344e06f8d5c3a85d20c587ffc93dfa3288043
Ricardo Mélida <ricardomelida92@gmail.com>
2019-06-09 05:19:42 -0400

    Se agrega condicional para quitar cancion seleccionada en la lista

--- src/ventanas/resultado.java
+++ src/ventanas/resultado.java
@@ -20,6 +20,7 @@ public class resultado extends javax.swing.JFrame {
         btn_listo = new javax.swing.JButton();
         btn_volver = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
+        btn_quitar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
@@ -53,6 +54,14 @@ public class resultado extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Cambria", 1, 18)); // NOI18N
         jLabel1.setText("PlayList Aleatorio");
 
+        btn_quitar.setFont(new java.awt.Font("Cambria", 1, 12)); // NOI18N
+        btn_quitar.setText("Quitar");
+        btn_quitar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_quitarActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -65,7 +74,9 @@ public class resultado extends javax.swing.JFrame {
                         .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                             .addComponent(btn_listo, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(btn_volver, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(btn_quitar, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(207, 207, 207)
+                            .addComponent(btn_volver, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 697, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(36, Short.MAX_VALUE))
         );
@@ -79,7 +90,9 @@ public class resultado extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(btn_listo, javax.swing.GroupLayout.DEFAULT_SIZE, 39, Short.MAX_VALUE)
-                    .addComponent(btn_volver, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(btn_volver, javax.swing.GroupLayout.DEFAULT_SIZE, 39, Short.MAX_VALUE)
+                        .addComponent(btn_quitar, javax.swing.GroupLayout.DEFAULT_SIZE, 39, Short.MAX_VALUE)))
                 .addContainerGap())
         );
 
@@ -203,12 +216,22 @@ public class resultado extends javax.swing.JFrame {
         this.setVisible(false);
     }//GEN-LAST:event_btn_volverActionPerformed
 
+    private void btn_quitarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_quitarActionPerformed
+        try {
+            tabla.removeRow(tabla_resultados.getSelectedRow());
+        }catch(Exception ex) {
+            JOptionPane.showMessageDialog(null,"Seleccione una fila para quital de las lista");
+        }
+        
+    }//GEN-LAST:event_btn_quitarActionPerformed
+
 
     
     conectar bd = new conectar();
     Connection db = bd.conexion();
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btn_listo;
+    private javax.swing.JButton btn_quitar;
     private javax.swing.JButton btn_volver;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane2;
