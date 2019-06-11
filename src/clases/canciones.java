10c8c1428be66190b593d2b2f4d83cc48a5a6b9a
Ricardo Mélida <ricardomelida92@gmail.com>
2019-06-11 08:43:12 -0400

    Se agrega clase para mejor dispocion de horarios en relacion a musicas

--- src/clases/canciones.java
+++ src/clases/canciones.java
@@ -8,18 +8,56 @@ import ventanas.conectar;
  *
  * @author ricky
  */
-public class resultado {
+public class canciones {
 
-    public resultado() {
+    public canciones() {
     
     }
     
-    public void datos(String estilo) {
+    public String[] madrugada(String estilo1, String estilo2) {
+        String [] datos = new String[6];
         try {
-            String sql = "SELECT * FROM musicas WHERE estilo = '" + estilo + "'";
+            String sql = "SELECT * FROM musicas WHERE estilo = '" + estilo1 + "' || estilo = '" + estilo2 + "' ORDER BY RAND()";
+            
+            
+            Statement st = db.createStatement();
+            ResultSet res = st.executeQuery(sql);
+            
+            while(res.next()) {
+                datos[0] = res.getString(1);
+                datos[1] = res.getString(2);
+                datos[2] = res.getString(3);
+                datos[3] = res.getString(4);
+                datos[4] = res.getString(5) + ":" + res.getString(6);
+            }
         }catch(Exception ex){
-            JOptionPane.showMessageDialog(null, "Error al cargar la musica");
+            JOptionPane.showMessageDialog(null, "Error al cargar la musica : " + ex);
         }
+        
+        return datos;
+    }
+    
+    public void temprano() {
+        
+    }
+    
+    public void mañana() {
+        
+    }
+    
+    public void m_dia() {
+        
+    }
+    
+    public void siesta() {
+        
+    }
+    
+    public void tarde() {
+        
+    }
+    public void noche() {
+        
     }
     
     conectar bd = new conectar();
